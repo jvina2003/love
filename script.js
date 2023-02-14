174c4dde6a1f080e7418ffb95d9b98fd57da53de
jvina2003 <103013654+jvina2003@users.noreply.github.com>
2023-02-14 00:05:47 +0000

    yay

--- script.js
+++ script.js
@@ -201,3 +201,38 @@ startButton.addEventListener(
     });
   })
 );
+
+
+//art page color change
+function bodyPaint() {
+	window.addEventListener("scroll", function () {
+		const scrollTop =
+			window.pageYOffset ||
+			document.documentElement.scrollTop ||
+			document.body.scrollTop ||
+			0;
+
+		const triggers = document.querySelectorAll("[data-color]");
+
+		const body = document.body;
+		
+		const scroll = scrollTop + window.innerHeight / 3;
+
+		for (var i = 0; i < triggers.length; i++) {
+			const trigger = triggers[i];
+			const triggerRect = trigger.getBoundingClientRect();
+			const triggerRectTop = triggerRect.top + scrollTop;
+			const triggerHeight = trigger.offsetHeight;
+			const triggerColor = trigger.getAttribute("data-color");
+
+			// if position is within range of this panel.
+			// So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+			if (triggerRectTop <= scroll && triggerRectTop + triggerHeight > scroll) {
+				body.setAttribute("data-paint", triggerColor);
+				console.log(body.dataset.paint);
+			}
+		}
+	});
+}
+
+bodyPaint();
